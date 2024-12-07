511301aa5ff41b6259abe0e3e0b5947651d5f0fc
Adailson Guimaraes <99360493+hada97@users.noreply.github.com>
2024-12-06 21:56:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://taxi-api-bahdbgddchbye7hu.canadacentral-01.azurewebsites.net";
+const baseUrl = "https://tax-docker-byaue6dfe3c0e0eq.canadacentral-01.azurewebsites.net";
 const apiUrlUsers = `${baseUrl}/users`;
 const apiUrlMotoristas = `${baseUrl}/drivers`;
 const apiUrlCorridas = `${baseUrl}/corridas`;
